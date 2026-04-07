231e069c16b2ae959e22c3c2710b77c93c75a143
Abhishek Kumar <abhishekmdp11@gmail.com>
2026-04-07 23:55:44 +0530

    implement fetch and display notes with card UI

--- components/NotesClient.jsx
+++ components/NotesClient.jsx
@@ -1,8 +1,10 @@
 "use client"
 import React, { useState } from 'react'
+import toast from 'react-hot-toast';
 
-function NotesClient() {
+function NotesClient({initialNotes}) {
 
+    const [notes, setNotes] = useState(initialNotes)
     const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
     const [loading, setLoading] = useState(false);
@@ -21,12 +23,17 @@ function NotesClient() {
                 body: JSON.stringify({title, content})
             })
             const result = await response.json();
-            console.log(result);
-            setTitle("");
-            setContent("");
+            // console.log(result);
+            if(result.success){
+                setNotes([result.data, ...notes])
+                toast.success("Notes created successfully")
+                setTitle("");
+                setContent("");
+            }
             setLoading(false);
         } catch (error) {
             console.error("Error createing note: ", error)
+            toast.error("Something went wrong")
         }
     }
 
@@ -62,6 +69,29 @@ function NotesClient() {
                     </button>
                 </div>
             </form>
+
+            <div className='space-y-4'>
+                <h2 className='text-xl font-semibold'>Your Notes ({notes.length})</h2>
+                {
+                    notes.length === 0 ? (<p>No Notes Yet. Create Your First Note Above</p>) : (
+                        notes.map((note) => (
+                            <div key={note._id} className='bg-white p-6 rounded-lg shadow-md'>
+                                <div className='flex justify-between items-start mb-2'>
+                                    <h3 className='text-lg font-semibold'>{note.title}</h3>
+                                    <div className='flex gap-2'>
+                                        <button className='text-blue-500 hover:text-blue-700 text-sm'>Edit</button>
+                                        <button className='text-red-500 hover:text-red-700 text-sm'>Delete</button>
+                                    </div>
+                                </div>
+                                <p className='text-gray-700 mb-2'>{note.content}</p>
+                                <p className='text-sm text-gray-500'>Created: {new Date(note.createdAt).toLocaleDateString()}</p>
+                                {note.updatedAt !== note.createdAt && (<p className='text-gray-500'>Updated: {new Date(note.updatedAt).toLocaleDateString()}</p>)}
+                            </div>
+                        ))
+                    )
+                }
+            </div>
+
         </div>
     )
 }
